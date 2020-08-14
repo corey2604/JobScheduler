2b4fbb8492c4c4cc3a4b7ea5d0f30ac0abaf424d
corey-duffy <coreyjduffy@hotmail.com>
2020-08-14 21:19:14 +0100

    Allow user to start a job via POST

--- src/main/java/prj/corey/jobscheduler/controllers/JobSchedulerController.java
+++ src/main/java/prj/corey/jobscheduler/controllers/JobSchedulerController.java
@@ -2,11 +2,13 @@ package prj.corey.jobscheduler.controllers;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import prj.corey.jobscheduler.jobs.PrintJob;
 import prj.corey.jobscheduler.jobs.SystemCallJob;
 import prj.corey.jobscheduler.jobs.WriteToFileJob;
+import prj.corey.jobscheduler.models.ScheduledJob;
 
 @RestController
 public class JobSchedulerController {
@@ -14,24 +16,35 @@ public class JobSchedulerController {
     private static final String SYSTEM_CALL_JOB_GROUP = "systemCallJobGroup";
     private static final String WRITE_TO_FILE_JOB_GROUP = "writeToFileJobGroup";
 
-    @GetMapping("/")
-    //TODO: This method exists for initial testing. To be replaced.
-    public String runJobs() {
-        JobDetail printJob = createJob(PrintJob.class,"printJob", PRINT_JOB_GROUP, "Test message");
-        Trigger printJobTrigger = createTrigger("printJobTrigger", PRINT_JOB_GROUP);
+    @PostMapping(path="/jobs", consumes="application/json")
+    public String runJob(@RequestBody ScheduledJob scheduledJob) {
 
-        JobDetail systemCallJob = createJob(SystemCallJob.class, "systemCallJob", SYSTEM_CALL_JOB_GROUP, "ls -a");
-        Trigger systemCallTrigger = createTrigger("systemCallTrigger", SYSTEM_CALL_JOB_GROUP);
+        //TODO: Setting default job to print job for testing
+        JobDetail job = createJob(PrintJob.class,"printJob", PRINT_JOB_GROUP, "User job was not run");
+        Trigger trigger = createTrigger("printJobTrigger", PRINT_JOB_GROUP);
 
-        JobDetail writeToFileJob = createJob(WriteToFileJob.class, "writeToFileJob", WRITE_TO_FILE_JOB_GROUP, "File content");
-        Trigger writeToFileTrigger = createTrigger("writeToFileTrigger", WRITE_TO_FILE_JOB_GROUP);
+        switch(scheduledJob.getType()) {
+            case "print":
+                job = createJob(PrintJob.class,"printJob", PRINT_JOB_GROUP, scheduledJob.getContent());
+                trigger = createTrigger("printJobTrigger", PRINT_JOB_GROUP);
+                break;
+            case "system_call":
+                job = createJob(SystemCallJob.class, "systemCallJob", SYSTEM_CALL_JOB_GROUP, scheduledJob.getContent());
+                trigger = createTrigger("systemCallTrigger", SYSTEM_CALL_JOB_GROUP);
+                break;
+            case "write_to_file":
+                job = createJob(WriteToFileJob.class, "writeToFileJob", WRITE_TO_FILE_JOB_GROUP, scheduledJob.getContent());
+                trigger = createTrigger("writeToFileTrigger", WRITE_TO_FILE_JOB_GROUP);
+                break;
+            default:
+                System.out.println("Unable to identify job to run");
+                break;
+        }
 
         try {
             Scheduler scheduler = new StdSchedulerFactory().getScheduler();
             scheduler.start();
-            scheduler.scheduleJob(printJob, printJobTrigger);
-            scheduler.scheduleJob(systemCallJob, systemCallTrigger);
-            scheduler.scheduleJob(writeToFileJob, writeToFileTrigger);
+            scheduler.scheduleJob(job, trigger);
         } catch (SchedulerException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/prj/corey/jobscheduler/models/ScheduledJob.java
@@ -0,0 +1,22 @@
+package prj.corey.jobscheduler.models;
+
+public class ScheduledJob {
+    private String type;
+    private String content;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
