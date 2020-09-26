fff4e910bb464c981276b889a204810f4be14e5c
corey-duffy <coreyjduffy@hotmail.com>
2020-09-26 09:19:28 +0100

    Class, method and variable name updates

--- src/main/java/prj/corey/jobscheduler/jobs/FileWriteJob.java
+++ src/main/java/prj/corey/jobscheduler/jobs/FileWriteJob.java
@@ -7,22 +7,22 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class WriteToFileJob extends AbstractJob {
+public class FileWriteJob extends AbstractJob {
     private static final String FILE_NAME = "jobSchedulerFile.txt";
 
     @Override
     public void execute(JobExecutionContext context) {
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
-        String fileContent = dataMap.getString(CONTENT);
+        String contentToWrite = dataMap.getString(CONTENT);
         try {
             File file = new File(FILE_NAME);
             if (file.createNewFile()) {
                 System.out.println("File created: " + file.getName());
             } else {
-                System.out.println(String.format("File \"%s\" already exists.", FILE_NAME));
+                System.out.printf("File \"%s\" already exists.%n", FILE_NAME);
             }
             FileWriter myWriter = new FileWriter(FILE_NAME);
-            myWriter.write(fileContent);
+            myWriter.write(contentToWrite);
             myWriter.close();
             System.out.println("Specified content has been written to file.");
         } catch (IOException e) {
